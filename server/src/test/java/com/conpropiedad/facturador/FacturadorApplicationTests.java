a2b763fff142aca0dc18ecf55ca1fe63662e7a17
jsuarez <jose.suarez@sophossolutions.com>
2020-10-18 23:58:54 -0500

    inicio back

---
+++ server/src/test/java/com/conpropiedad/facturador/FacturadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conpropiedad.facturador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
